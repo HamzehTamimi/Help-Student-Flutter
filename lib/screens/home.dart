edbbe08fb72a007e1aae771b9a7e451e4998b222
HamzehTamimi <63554013+HamzehTamimi@users.noreply.github.com>
2025-01-04 11:45:32 +0300

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,37 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:helpstudent/cards/cards.dart';
+import 'package:helpstudent/screens/settings.dart';
 
 class Homescreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        bottomNavigationBar: BottomNavigationBar(items: const <BottomNavigationBarItem>[
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              label: 'Settings',
+            icon: Icon(Icons.settings),
+            label: 'Settings',
           ),
-        ]),
-        appBar: AppBar(
-          title: const Text('Help Student'),
-          backgroundColor: Color.fromRGBO(187, 222, 251, 1),
-          centerTitle: true,
-        ),
-        body: GridView.count(
-          crossAxisCount: 1,
-          childAspectRatio: (1 / .7),
-          children: <Widget>[
-              GuidanceCard(),
-              ITSpecialtyCard(),
-              GPACard(),
-              MapCard(),
-              CVCard(),
-          ],
-        ),
-      );
+        ],
+        onTap: (int index) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => index == 0 ? Homescreen() : SettingsPage(),
+            ),
+          );
+        },
+      ),
+      appBar: AppBar(
+        title: const Text('Help Student'),
+        backgroundColor: Color.fromRGBO(187, 222, 251, 1),
+        centerTitle: true,
+      ),
+      body: GridView.count(
+        crossAxisCount: 1,
+        childAspectRatio: (1 / .7),
+        children: <Widget>[
+          GuidanceCard(),
+          ITSpecialtyCard(),
+          GPACard(),
+          MapCard(),
+          CVCard(),
+        ],
+      ),
+    );
   }
 }
