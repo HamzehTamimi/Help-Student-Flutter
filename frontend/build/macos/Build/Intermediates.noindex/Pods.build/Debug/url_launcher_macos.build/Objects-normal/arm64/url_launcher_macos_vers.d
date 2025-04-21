8dc4cd403a3df9d69cb8d97485fa8b51728ba3b9
HamzehTamimi <63554013+HamzehTamimi@users.noreply.github.com>
2025-04-21 16:29:01 +0300

    fix

--- frontend/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/Objects-normal/arm64/url_launcher_macos_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hamzehtamimi/Documents/GitHub/Help-Student-Flutter/frontend/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/module.modulemap \
-  /Users/hamzehtamimi/Documents/GitHub/Help-Student-Flutter/frontend/build/macos/Build/Intermediates.noindex/Pods.build/Debug/url_launcher_macos.build/DerivedSources/url_launcher_macos_vers.c
